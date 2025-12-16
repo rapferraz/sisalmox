824fcf3b8d69a0fff376ceb2552c1686e5f4b9c0
Raphael Ferraz <rapferrazsbsp@gmail.com>
2025-12-16 16:22:47 -0300

    Deletar arquivos antigos

--- sisalmox/src/main/java/br/com/alfatecmarine/sisalmox/Exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package br.com.alfatecmarine.sisalmox.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    
-    public ResourceNotFoundException(String msgErro) {
-        super(msgErro);
-    }
-}
